d1c660fc8611b7de46af1e79c5b40e5c1829be3d
[Abbaskalith] <[abkalith@gmail.com]>
2021-10-10 21:48:55 +0530

    Added Employeewage Computation

---
+++ Employeewage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employeewage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage Computation");
+        }
+    }
+}
