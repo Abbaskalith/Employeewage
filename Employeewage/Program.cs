6d5af70ce834b82a8d654c0c868df2264d6afdef
[Abbaskalith] <[abkalith@gmail.com]>
2021-10-10 22:28:26 +0530

    Added Use case 1

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Employeewage
+namespace EmployeewageUC1
 {
-    class Program
+    class EmployeewageCompute
     {
+        /// <summary>
+        /// Entry point of the applicaiton.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewage Computation");
+            //Constant
+            int IS_PRESENT = 1;
+            //Generating random input using predefined Random class
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            //if-else construct
+            if (IS_PRESENT == empInput)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
