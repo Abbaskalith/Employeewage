ea2ac0f4e06c3fe543cef7d774e20e35fe9d42cd
[Abbaskalith] <[abkalith@gmail.com]>
2021-10-10 22:57:12 +0530

    Added daily wage calculation

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -4,26 +4,32 @@ namespace EmployeewageUC1
 {
     class EmployeewageCompute
     {
-        /// <summary>
-        /// Entry point of the applicaiton.
-        /// </summary>
-        /// <param name="args"></param>
+        /// Contants
+        public const int EMP_RATE_PER_HR = 20;
         static void Main(string[] args)
         {
             //Constant
-            int IS_PRESENT = 1;
+            int PRESENT = 1;
+            //Local varibles
+            int empHrs = 0;
+            int empWage = 0;
             //Generating random input using predefined Random class
             Random random = new Random();
             int empInput = random.Next(0, 2);
             //if-else construct
-            if (IS_PRESENT == empInput)
+            if (PRESENT == empInput)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Wage:{0}", empWage);
+            Console.ReadLine();
         }
     }
 }
