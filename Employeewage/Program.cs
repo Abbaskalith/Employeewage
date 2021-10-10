2620f424f1358d0dcf03494e3341d641d20d6695
[Abbaskalith] <[abkalith@gmail.com]>
2021-10-10 23:26:20 +0530

    Added part time employee calculation

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -1,34 +1,42 @@
 ﻿using System;
 
-namespace EmployeewageUC1
+namespace EmployeewageUC3
 {
     class EmployeewageCompute
     {
         /// Contants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HR = 20;
         static void Main(string[] args)
         {
-            //Constant
-            int PRESENT = 1;
             //Local varibles
             int empHrs = 0;
             int empWage = 0;
+
             //Generating random input using predefined Random class
             Random random = new Random();
-            int empInput = random.Next(0, 2);
+            int empInput = random.Next(0, 3);
+
             //if-else construct
-            if (PRESENT == empInput)
+            if (empInput == 1)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is present full time");
                 empHrs = 8;
+            }
+            else if (empInput == 2)
+            {
+                Console.WriteLine("Employee is present part time");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
                 empHrs = 0;
             }
+            //Calculating daily employee wage
             empWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Employee Wage:{0}", empWage);
+            Console.WriteLine("Employee wage:{0}", empWage);
             Console.ReadLine();
         }
     }
